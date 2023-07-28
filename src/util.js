4925b65c8fb94da4c0a81a701f62318fd67b5a38
seorinn <seorin@ajou.ac.kr>
2023-07-29 01:26:27 +0900

    Test: Add testind imgs

--- src/util.js
+++ src/util.js
@@ -1,19 +1,21 @@
-import img0 from "./img/logo192.png";
+import buldak from "./img/buldak.png";
+import dduk from "./img/dduk.png";
+import ramen from "./img/ramen.png";
 
 export const getSelectionTextById = (selectionId) => {
     const targetSelectionId = String(selectionId);
     switch(targetSelectionId){
-        case "0":
-            return "이게 맵다고?🍼";
         case "1":
-            return "딱 맛있게 맵다☺️";
+            return "이게 맵다고?🍼";
         case "2":
-            return "맵지만 즐길 수는 있다🌶️";
+            return "딱 맛있게 맵다☺️";
         case "3":
+            return "맵지만 즐길 수는 있다🌶️";
+        case "4":
             return "휴 도전이었다💦";
-        case "4": 
-            return "다시는 못 먹을 매운맛!!🔥";
         case "5": 
+            return "다시는 못 먹을 매운맛!!🔥";
+        case "6": 
             return "먹어본 적 없다🤷‍♀️";
         default:
             return null;
@@ -24,21 +26,17 @@ export const getItemImgById = (itemId) => {
     const targetItemId = String(itemId);
     switch(targetItemId){
         case "0":
-            return img0;
+            return ramen;
         case "1":
-            return img0;
+            return buldak;
         case "2":
-            return img0;
+            return dduk;
         default:
             return null;
     }
 }
 
 export const selectionList = [
-    {
-        id: 0,
-        name: getSelectionTextById(0),
-    },
     {
         id: 1,
         name: getSelectionTextById(1),
@@ -58,6 +56,10 @@ export const selectionList = [
     {
         id: 5,
         name: getSelectionTextById(5),
+    },    
+    {
+        id: 6,
+        name: getSelectionTextById(6),
     },
 ]
 
@@ -77,5 +79,17 @@ export const testItemList = [
         itemName: "엽기떡볶이 오리지널",
         img: getItemImgById(2),
     },
-
 ]
+
+export const getMonthRangeByDate = (date) => {
+    const beginTimeStamp = new Date(date.getFullYear(), date.getMonth, 1).getTime();
+    const endTimeStamp = new Date(
+        date.getFullYear(),
+        date.getMonth() +1,
+        0,
+        23,
+        59,
+        59
+    ).getTime();
+    return { beginTimeStamp, endTimeStamp };
+}
