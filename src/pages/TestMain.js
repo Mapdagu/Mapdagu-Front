31cbbd7accf269d90e68dc65099b01670d359ec7
seorinn <seorin@ajou.ac.kr>
2023-08-01 22:58:39 +0900

    Modify navigate to Login page

--- src/pages/TestMain.js
+++ src/pages/TestMain.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const TestMain = () => {
     const navigate = useNavigate();
     const goTest = () => {
-        navigate(`/test`);
+        navigate(`/login`);
     }
     return (
         <div>
