3fb1d7b90581796e6f717f22151bb0e06a7df309
seorinn <seorin@ajou.ac.kr>
2023-08-05 22:11:09 +0900

    Add proxy and modify redirect url

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,16 +12,12 @@ const Login = ({onSubmit}) => {
         password: "",
     });
 
-    // const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
-    // const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
-    // const GOOGLE_CLIENT_ID = `131547171304-i2ek49afir8ad0eeufsghep9tji5e2bs.apps.googleusercontent.com`;
     const REST_API_KEY = config.KAKAO_API_KEY;
     const NAVER_CLIENT_ID = config.NAVER_CLIENT_ID;
     const GOOGLE_CLIENT_ID = config.GOOGLE_CLIENT_ID;
-    console.log(config.KEY);
     
-    const KAKAO_REDIRECT_URI = `http://localhost:3000/oauth/callback/kakao`;
-    const NAVER_REDIRECT_URI = `http://localhost:3000/oauth/callback/naver`;
+    const KAKAO_REDIRECT_URI = `http://localhost:3000/oauth2/code/kakao`;
+    const NAVER_REDIRECT_URI = `http://localhost:3000/oauth2/code/naver`;
     const STATE = "flase";
     const kakaoLink = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
     const naverLink = `http://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${NAVER_REDIRECT_URI}`
