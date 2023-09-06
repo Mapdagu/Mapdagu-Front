b7b046b958e1b37d6041cf20bac1817e5cb36aff
seorinn <seorin@ajou.ac.kr>
2023-09-06 22:14:03 +0900

    Move Mypage icon from navigator to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,35 @@
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
-const Header = ({title, leftChild, rightChild}) => {
-    return (
-        <div className="Header">
-            <div className="header_left">{leftChild}</div>
-            <div className="header_title">{title}</div>
-            <div className="header_right">{rightChild}</div>
-        </div>
-    )
+const Header = ({type, title, leftChild, rightChild}) => {
+    const navigate = useNavigate();
+    
+    const changePage = () => {
+        navigate(`/`);
+      }
+
+    if(type === 1){
+        return (
+            <div className="Header">
+                <div className="header_left">
+                    <button>내가 맵다 했지!</button></div>
+                <div className="header_title">
+                    <button onClick={changePage}>처음으로</button></div>
+                <div className="header_right">
+                    <button>🔍</button>
+                    <button>🙂</button>
+                </div>
+            </div>
+        )        
+    } else {
+        return (
+            <div className="Header">
+                <div className="header_left">{leftChild}</div>
+                <div className="header_title">{title}</div>
+                <div className="header_right">{rightChild}</div>
+            </div>
+        )
+    } 
 }
 
 export default Header;
--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -21,7 +21,7 @@ const Navigator = () => {
             <button onClick={goMain}>홈</button> 
             <button onClick={goFriend}>친구</button> 
             <button onClick={goEval}>맵기평가</button> 
-            <button onClick={goMyPage}>마이페이지</button>           
+            {/* <button onClick={goMyPage}>마이페이지</button>            */}
         </div>
     )
 }
