c50ef75fdcf50a95fea30abdfcd2e27b70528f5b
seorinn <seorin@ajou.ac.kr>
2023-10-24 17:02:53 +0900

    Refactor: Reload in RequestModal

--- src/components/friend/RequestModal.js
+++ src/components/friend/RequestModal.js
@@ -8,7 +8,7 @@ const RequestModal = ({closeModal, requestData, deleteRequest , manageFriend}) =
     return(
         <div className="RequestModal">
             <div className="friend_modal_title">친구 요청</div>
-                    <div><button className="btn_profile_close" onClick={closeModal}>×</button></div>
+                    <div><button className="btn_modal_close" onClick={closeModal}>×</button></div>
                     <div className="request_list_wrapper">
                     { !requestData || requestData.length === 0 ?
                     <p>받은 요청이 없습니다</p> :
